d2d60746a96a0f520ce62b65b5ed1bcc57ec3602
Italo <italopardal@hotmail.com>
2021-05-29 10:09:24 +0100

    working react scripts and linked to project

--- music_controller/frontend/views.py
+++ music_controller/frontend/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Going to render the index template and let react take over
+
+
+def index(request, *args, **kwargs):
+    return render(request, "frontend/index.html")
