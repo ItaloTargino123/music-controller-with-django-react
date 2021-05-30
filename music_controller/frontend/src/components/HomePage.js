4a2dd2541e61733a7d4c0db331d3466555096834
Italo <italopardal@hotmail.com>
2021-05-30 10:34:49 +0100

    create a new view called room to enter the room that was created

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -8,6 +8,8 @@ import {
     Link,
     Redirect,
 } from "react-router-dom";
+import Room from "./Room";
+
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -23,6 +25,7 @@ export default class HomePage extends Component {
                     </Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         );
